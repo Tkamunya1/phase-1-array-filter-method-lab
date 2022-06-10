f36a01b753911b9bfb1974248ce49af1ee58625a
Titus <titus.kamunya@student.moringaschool.com>
2022-06-10 12:05:54 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(names, searchName){
+    return names.filter(driver => searchName.toLowerCase() === driver.toLowerCase())
+  }
+  function fuzzyMatch(names, searchStr){
+    return names.filter(driver => driver.startsWith(searchStr))
+  }
+  function matchName(names, nameStr){
+    return names.filter(driver => driver.name === nameStr)
+  } 
